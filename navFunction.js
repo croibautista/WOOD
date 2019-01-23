2e46e285e1ebb7b331b5dc0808258bb458ff0761
Croi Bautista <christ.bautista@lt000199.wideout.com>
2019-01-24 06:45:57 +0800

    1/24/2019

--- navFunction.js
+++ navFunction.js
@@ -68,24 +68,3 @@ $('#myDropdown').on('show.bs.popover', function () {
   console.log("dddddd")
 })
 
-
-
-
-$(function(){
-    
-  // Enabling Popover Example 1 - HTML (content and title from html tags of element)
-  $("[data-toggle=popover]").popover();
-
-  // Enabling Popover Example 2 - JS (hidden content and title capturing)
-  $("#popoverExampleTwo").popover({
-      html : true, 
-      content: function() {
-        return $('#popoverExampleTwoHiddenContent').html();
-      },
-      title: function() {
-        return $('#popoverExampleTwoHiddenTitle').html();
-      }
-  });
-
-});
-
